b0a7e325ecb65f69509c0e3db21edd71277c59e3
mjaylene <mjaylene@stanford.edu>
2023-12-27 19:23:55 -0800

    finished coming soon

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const handleIntersection = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting && entry.target.classList.contains('b')) {
       document.getElementById('title-heading').innerText = 'is an ...';
+      $(document).ready(function() {
+        $(".title.b").hover(
+          function() {
+            $(".intro-container .profile-picture").css("background", "url('images/artistprofile3.png')");
+          },
+          function() {
+            $(".intro-container .profile-picture").css("background", "url('images/normalprofile.png')");
+          }
+        );
+      });
+      
     } else if (!entry.isIntersecting && entry.target.classList.contains('b')) {
       const text = document.getElementById('title-heading').innerText;
       document.getElementById('title-heading').innerText = 'is a ...';
@@ -20,3 +31,28 @@ const artistElement = document.querySelector('.title.b');
 
 // Observe the target element
 observer.observe(artistElement);
+
+
+$(document).ready(function() {
+  $(".title.c").hover(
+    function() {
+      $(".intro-container .profile-picture").css("background", "url('images/graphicdesignprofile.png')");
+    },
+    function() {
+      $(".intro-container .profile-picture").css("background", "url('images/normalprofile.png')");
+    }
+  );
+});
+
+
+$(document).ready(function() {
+  $(".title.a").hover(
+    function() {
+      $(".intro-container .profile-picture").css("background", "url('images/softwareprofile.png')");
+    },
+    function() {
+      $(".intro-container .profile-picture").css("background", "url('images/normalprofile.png')");
+    }
+  );
+});
+
